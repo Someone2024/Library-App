fcde7aa37fd3c3718b2f6f8907a5309062aa265f
Someone2024 <131807216+Someone2024@users.noreply.github.com>
2023-06-13 05:37:43 -0500

    Feat: new auth system and auth state changes

--- src/server/server.js
+++ src/server/server.js
@@ -1,8 +1,24 @@
-import { SignIn, SignOut } from "./Authentication";
+import { SignIn, SignOut, auth } from "./Authentication";
 import { onAuthStateChanged } from "firebase/auth";
 
 const LoginButton = document.querySelector(".login");
 
-LoginButton.addEventListener("click", ()=> {
-    SignIn()
-})
+const LogOutButton = document.querySelector(".logout");
+
+LoginButton.addEventListener("click", () => SignIn())
+LogOutButton.addEventListener("click", () => SignOut())
+
+onAuthStateChanged(auth, (user) => {
+  if(user) {
+    // User is signed in, see docs for a list of available properties
+    // https://firebase.google.com/docs/reference/js/auth.user
+    LogOutButton.style.display = "block"
+    LoginButton.style.display = "none"
+    // ...
+  } else {
+    // User is signed out
+    // ...
+    LogOutButton.style.display = "none"
+    LoginButton.style.display = "block"
+  }
+});
