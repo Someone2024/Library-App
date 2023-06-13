99e4b62030896788f5d9978ec9b5ee527b1f53ef
Someone2024 <131807216+Someone2024@users.noreply.github.com>
2023-06-13 05:36:00 -0500

    Update Authentication.js
    exposed the auth object

--- src/server/Authentication.js
+++ src/server/Authentication.js
@@ -30,19 +30,8 @@ function SignOut() {
       });
 }
 
-const AuthState = onAuthStateChanged(auth, (user) => {
-  if (user) {
-    // User is signed in, see docs for a list of available properties
-    // https://firebase.google.com/docs/reference/js/auth.user
-    const uid = user.uid;
-    // ...
-  } else {
-    // User is signed out
-    // ...
-  }
-});
-
 export {
     SignIn,
-    SignOut
+    SignOut,
+    auth
 }
