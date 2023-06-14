67956f199c51da3d2787a53d79f3934213dd99f7
santiago <bassilsantiago@gmail.com>
2023-06-14 14:08:23 -0500

    Todo: show the iser profile picture

--- src/server/server.js
+++ src/server/server.js
@@ -4,6 +4,7 @@ import { onAuthStateChanged } from "firebase/auth";
 const LoginButton = document.querySelector(".login");
 
 const LogOutButton = document.querySelector(".logout");
+const Profile = document.querySelector(".profile");
 
 LoginButton.addEventListener("click", () => SignIn())
 LogOutButton.addEventListener("click", () => SignOut())
@@ -13,12 +14,15 @@ onAuthStateChanged(auth, (user) => {
     // User is signed in, see docs for a list of available properties
     // https://firebase.google.com/docs/reference/js/auth.user
     LogOutButton.style.display = "block"
+    Profile.style.display = "block"
+    Profile.src = "set user profile pic here"
     LoginButton.style.display = "none"
     // ...
   } else {
     // User is signed out
     // ...
     LogOutButton.style.display = "none"
+    Profile.style.display = "block"
     LoginButton.style.display = "block"
   }
 });
