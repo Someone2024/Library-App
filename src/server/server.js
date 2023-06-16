d09973548e307b0aad82a258853aa4da40e27a8c
santiago <bassilsantiago@gmail.com>
2023-06-15 21:59:40 -0500

    Feat: display current user's profile picture

--- src/server/server.js
+++ src/server/server.js
@@ -1,10 +1,7 @@
-import { SignIn, SignOut, auth } from "./Authentication";
+import { SignIn, SignOut, auth, user } from "./Authentication";
 import { onAuthStateChanged } from "firebase/auth";
-
-const LoginButton = document.querySelector(".login");
-
-const LogOutButton = document.querySelector(".logout");
-const Profile = document.querySelector(".profile");
+import {ProfilePic, LoginButton, LogOutButton} from "../App"
+import createBook, {retrieveBooks} from  "./CloudStore"
 
 LoginButton.addEventListener("click", () => SignIn())
 LogOutButton.addEventListener("click", () => SignOut())
@@ -13,16 +10,15 @@ onAuthStateChanged(auth, (user) => {
   if(user) {
     // User is signed in, see docs for a list of available properties
     // https://firebase.google.com/docs/reference/js/auth.user
-    LogOutButton.style.display = "block"
-    Profile.style.display = "block"
-    Profile.src = "set user profile pic here"
     LoginButton.style.display = "none"
-    // ...
+    LogOutButton.style.display = "block"
+    ProfilePic.style.display = "inline"
+    ProfilePic.src= user.photoURL
   } else {
     // User is signed out
     // ...
     LogOutButton.style.display = "none"
-    Profile.style.display = "block"
+    ProfilePic.style.display = "none"
     LoginButton.style.display = "block"
   }
 });
