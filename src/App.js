d8237e3fb6b8e5c932230960f81aefd639dfdb65
santiago <bassilsantiago@gmail.com>
2023-06-16 21:19:05 -0500

    Feat: create helper functions for things like show error message when the user is loged out

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import {addBookToLibrary, displayBooks} from "./scripts/main"
+import { addBookToLibrary, displayBooks } from "./scripts/main"
+import { auth } from "./server/Authentication";
+import createBook, { retrieveBooks } from "./server/CloudStore"
 
 const newBook = document.getElementById("new-book");
 const bookForm = document.getElementById("book-form");
 const addBook = document.getElementById("add-book");
-const dialog = document.querySelector(".dialog"); 
+const dialog = document.querySelector(".dialog");
+
+const ProfilePic = document.querySelector(".profile")
+
+const LoginButton = document.querySelector(".login");
+
+const LogOutButton = document.querySelector(".logout");
+
+const AuthErr = document.querySelector(".authErr");
+const CloseErr = document.querySelector(".closeErr");
 
 const bookFields = {
   title: document.getElementById("title"),
@@ -12,20 +23,36 @@ const bookFields = {
   read: document.getElementById("read"),
 };
 
-newBook.addEventListener("click", () => {
+function showModal() {
   dialog.showModal()
-});
+}
 
-addBook.addEventListener("click", () => {
-  addBookToLibrary(
-    bookFields.title.value,
-    bookFields.author.value,
-    bookFields.pages.value,
-    bookFields.read.checked,
-  );
+function newBookEvent() {
+  newBook.addEventListener("click", showModal)
+}
 
-  displayBooks();
-  bookForm.reset()
+function deleteNewBookEvent() {
+  newBook.removeEventListener("click", showModal)
+}
 
-  dialog.close()
-})
+function showAuthErr() {
+  AuthErr.showModal()
+}
+
+function closeAuthErr() {
+  AuthErr.close()
+}
+
+function authErr() {
+  newBook.addEventListener("click", showAuthErr)
+  CloseErr.addEventListener("click", closeAuthErr)
+}
+
+export {
+  ProfilePic,
+  LoginButton,
+  LogOutButton,
+  newBookEvent,
+  deleteNewBookEvent,
+  authErr,
+}
