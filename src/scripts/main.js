343a5c67d434764987cc41e0f056a2f984c271c1
Someone <bassilsantiago@gmail.com>
2023-06-17 08:08:02 -0500

    Update: remove unnecessary functions

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,3 @@
-let myLibrary = [];
-
 class Book {
   constructor(title, author, pages, read) {
     (this.title = title),
@@ -16,69 +14,4 @@ class Book {
   getTitle = () => this.title;
   getAuthor = () => this.author;
   getPages = () => this.pages;
-}
-
-function addBookToLibrary(title, author, pages, read) {
-  const newBook = new Book(title, author, pages, read);
-  myLibrary = [newBook];
-}
-
-function displayBooks() {
-  const cardsContainer = document.querySelector(".cards-container")
-  myLibrary.forEach((book) => {
-    const card = document.createElement("div");
-    const cardTitle = document.createElement("p");
-    const cardAuthor = document.createElement("p");
-    const cardPages = document.createElement("p");
-    const removeBook = document.createElement("button");
-    const readStatus = document.createElement("button");
-
-    cardTitle.textContent = '"' + book.title + '"'; 
-    cardAuthor.textContent = book.author; 
-    cardPages.textContent = book.pages; 
-    card.append(cardTitle, cardAuthor, cardPages);
-
-    removeBook.textContent = "Remove";
-    card.appendChild(readStatus);
-    card.appendChild(removeBook);
-    card.classList.add("card");
-
-    readStatus.classList.add("read")
-    removeBook.classList.add("remove")
-
-    if (book.read === true){
-      readStatus.textContent = "Read";
-    } 
-    else readStatus.textContent = "Not read";
-
-    if(readStatus.textContent === "Not read"){
-      readStatus.style.backgroundColor = "#ff9c9c";
-    }else{
-      readStatus.style.backgroundColor = "#9fff9c";
-    }
-      
-
-    readStatus.addEventListener("click", () => {
-      if (readStatus.textContent === "Not read"){
-        readStatus.textContent = "Read";
-        readStatus.style.backgroundColor = "#9fff9c";
-      }
-      else {readStatus.textContent = "Not read";
-           readStatus.style.backgroundColor = "#ff9c9c";
-          }
-
-    });
-
-    removeBook.addEventListener("click", () => {
-      cardsContainer.removeChild(card);
-      myLibrary.pop(); // <-- This may cause problems
-    });
-
-    cardsContainer.append(card);
-  });
-}
-
-export {
-  addBookToLibrary,
-  displayBooks
 }
