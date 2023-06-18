269263975e78448986e9b4b77cfa1730c291750b
santiago <bassilsantiago@gmail.com>
2023-06-18 06:07:35 -0500

    BugFix: split the retrieve books function to not display duplicated books

--- src/server/CloudStore.js
+++ src/server/CloudStore.js
@@ -6,6 +6,7 @@ import {
   getDocs,
   addDoc,
   deleteDoc,
+  doc
 } from "firebase/firestore";
 import app from "./firebaseApp";
 
@@ -20,61 +21,67 @@ export default async function createBook(title, author, pages, read, userId) {
       read: read,
       id: userId,
     });
+    const q = doc(collection(db, "Books"), where("id", "==", userId));
+    const doc = await getDoc(q)
+    createCard(doc.data().title, doc.data().author, doc.data().pages, doc.data().read, doc.ref)
     console.log("Document written with ID: ", docRef.id);
-  } catch (err) {
+  } catch(err) {
     console.log("Error adding document: ", err);
   }
 }
 
 export async function retrieveBooks(currentUserId) {
   const q = query(collection(db, "Books"), where("id", "==", currentUserId));
-  const cardsContainer = document.querySelector(".cards-container");
 
   const querySnapshot = await getDocs(q);
   querySnapshot.forEach((doc) => {
     // doc.data() is never undefined for query doc snapshots
     //take books info from doc.data()
-
-    const card = document.createElement("div");
-    const cardTitle = document.createElement("p");
-    const cardAuthor = document.createElement("p");
-    const cardPages = document.createElement("p");
-    const removeBook = document.createElement("button");
-    const readStatus = document.createElement("button");
-
-    cardTitle.textContent = '"' + doc.data().title + '"';
-    cardAuthor.textContent = doc.data().author;
-    cardPages.textContent = doc.data().pages;
-    card.append(cardTitle, cardAuthor, cardPages, readStatus,removeBook);
-
-    card.classList.add("card");
-    removeBook.textContent = "Remove"
-
-    if (doc.data().read === true) {
-      readStatus.textContent = "Read";
-    } else readStatus.textContent = "Not read";
-
-    if (readStatus.textContent === "Not read") {
-      readStatus.style.backgroundColor = "#ff9c9c";
-    } else {
-      readStatus.style.backgroundColor = "#9fff9c";
-    }
-
-    readStatus.addEventListener("click", () => {
-      if (readStatus.textContent === "Not read") {
-        readStatus.textContent = "Read";
-        readStatus.style.backgroundColor = "#9fff9c";
-      } else {
-        readStatus.textContent = "Not read";
-        readStatus.style.backgroundColor = "#ff9c9c";
-      }
-    });
-
-    removeBook.addEventListener("click", async () => {
-      cardsContainer.removeChild(card);
-      await deleteDoc(doc.ref)
-    });
-
-    cardsContainer.append(card);
+    createCard(doc.data().title, doc.data().author, doc.data().pages, doc.data().read, doc.ref)
   });
+}
+
+export async function createCard(title, author, pages, read, docRef) {
+  const cardsContainer = document.querySelector(".cards-container");
+  const card = document.createElement("div");
+  const cardTitle = document.createElement("p");
+  const cardAuthor = document.createElement("p");
+  const cardPages = document.createElement("p");
+  const removeBook = document.createElement("button");
+  const readStatus = document.createElement("button");
+
+  cardTitle.textContent = '"' + title + '"';
+  cardAuthor.textContent = author;
+  cardPages.textContent = pages;
+  card.append(cardTitle, cardAuthor, cardPages, readStatus, removeBook);
+
+  card.classList.add("card");
+  removeBook.textContent = "Remove"
+
+  if(read === true) {
+    readStatus.textContent = "Read";
+  } else readStatus.textContent = "Not read";
+
+  if(readStatus.textContent === "Not read") {
+    readStatus.style.backgroundColor = "#ff9c9c";
+  } else {
+    readStatus.style.backgroundColor = "#9fff9c";
+  }
+
+  readStatus.addEventListener("click", () => {
+    if(readStatus.textContent === "Not read") {
+      readStatus.textContent = "Read";
+      readStatus.style.backgroundColor = "#9fff9c";
+    } else {
+      readStatus.textContent = "Not read";
+      readStatus.style.backgroundColor = "#ff9c9c";
+    }
+  });
+
+  removeBook.addEventListener("click", async () => {
+    cardsContainer.removeChild(card);
+    await deleteDoc(docRef)
+  });
+
+  cardsContainer.append(card);
 }
